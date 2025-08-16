cba40733fbcd5eb0f95c4e129f62164feba9e433
firefried-fries <102931771+firefried-fries@users.noreply.github.com>
2025-08-16 20:55:30 +1000

    Linking creation of new game and new channel together

--- src/commands/add-self.ts
+++ src/commands/add-self.ts
@@ -5,11 +5,11 @@ import { GameManagement } from "../state-management";
 export const data = new SlashCommandBuilder()
   .setName("addself")
   .setDescription("add self to current game").addStringOption((option) =>
-    option.setName("Game ID").setDescription("ID of the game to add yourself to")
+    option.setName("gameid").setDescription("ID of the game to add yourself to")
   );
 
 export async function execute(interaction, state: GameManagement) {
-  const gameID = interaction.getString("Game ID") ?? state.activeGame?.gameID;
+  const gameID = interaction.getString("gameid") ?? state.activeGame?.gameID;
   const game = state.getGameFromID(gameID);
   if (game === null) {
     return interaction.reply(constants.noActiveGame);
