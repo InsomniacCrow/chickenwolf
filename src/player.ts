c01b177bfa126bcedb4ed259dad793fafaf1ec01
firefried-fries <102931771+firefried-fries@users.noreply.github.com>
2025-08-16 15:59:31 +1000

    Modified player.ts to accomedate the changes in other classes.

--- src/player.ts
+++ src/player.ts
@@ -1,30 +1,33 @@
 enum GroupProperties {
-  CanVote = 1,
-  CanKill,
-  CanSheild
+  Vote = "vote",
+  CanKill = "kill",
+  CanSheild = "shield"
 }
 
 export class Group {
-  private game_id: number;
+  private game_id: string;
   private players: Player[];
-  private properties: number[];
+  private properties: Map<string, boolean|number|string>;
+  public readonly acts_in_day: boolean;
 
-  public constructor(game_id: number) {
+  // By default the group acts in day
+  public constructor(game_id: string, acts_in_day: boolean = true) {
     this.game_id = game_id;
     this.players = this.players;
-    this.properties = this.properties;
+    this.properties = new Map<string, boolean|number|string>();
+    this.acts_in_day = acts_in_day;
   }
 
-  public getGameId(): number{
+  public getGameId(): string{
     return this.game_id;
   }
 
-  public getProperties(): number[] {
+  public getProperties(): Map<string, boolean|number|string> {
     return this.properties;
   }
 
-  public addProperties(property: number) {
-    this.properties.push(property);
+  public addProperties(property: string, value: boolean|number|string) {
+    this.properties.set(property, value);
   }
 
   public getPlayers(): Player[] {
@@ -37,12 +40,12 @@ export class Group {
  *  Basic player class
  */
 export class Player {
-  private game_id: number; // id of the game player belongs to
+  private game_id: string; // id of the game player belongs to
   private user_id: number; 
   private groups: Group[];
 
-  public constructor(game_id: number, user_id: number) {
-    this.game_id = game_id
+  public constructor(game_id: string, user_id: number) {
+    this.game_id = game_id;
     this.user_id = user_id;
     this.groups = []; // starts with empty group id.
   }
@@ -51,7 +54,7 @@ export class Player {
     return this.user_id;
   }
 
-  public getGameId(): number {
+  public getGameId(): string {
     return this.game_id;
   }
 
