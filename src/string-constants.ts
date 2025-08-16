7119b70b40f2ea7884c6418fe171578afd026be2
gareyyy <gk10guce@gmail.com>
2025-08-16 18:31:00 +1000

    we killing players more

--- src/string-constants.ts
+++ src/string-constants.ts
@@ -1,2 +1,5 @@
 export const playerList = "playerList";
 export const noActiveGame = "No active game :(";
+export const living = "living";
+export const werewolf = "werewolf";
+export const villager = "villager";
