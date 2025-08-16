d24b2ca6c1b10c6def6a08d45ae41b1be0cba527
gareyyy <gk10guce@gmail.com>
2025-08-16 19:31:57 +1000

    reimplemented addself

--- src/commands/add-self.ts
+++ src/commands/add-self.ts
@@ -1,17 +1,21 @@
-import { CommandInteraction, InteractionType, SlashCommandBuilder, User } from "discord.js";
+import { CommandInteraction, CommandInteractionOptionResolver, InteractionType, SlashCommandBuilder, User } from "discord.js";
 import * as constants from "../string-constants";
 import { GameManagement } from "../state-management";
 
 export const data = new SlashCommandBuilder()
   .setName("addself")
-  .setDescription("add self to current game");
+  .setDescription("add self to current game").addStringOption((option) =>
+    option.setName("Game ID").setDescription("ID of the game to add yourself to")
+  );
 
-export async function execute(interaction: CommandInteraction, state: GameManagement) {
-  if (state.activeGame === null) {
+export async function execute(interaction, state: GameManagement) {
+  const gameID = interaction.getString("Game ID") ?? state.activeGame?.gameID;
+  const game = state.getGameFromID(gameID);
+  if (game === null) {
     return interaction.reply(constants.noActiveGame);
   }
   try {
-    state.activeGame.addPlayer(interaction.user);
+    game.addPlayer(interaction.user);
     await interaction.reply(`Added ${interaction.user}`);
   } catch (error) {
     return interaction.reply("Player already in list");
--- src/commands/remove-self.ts
+++ src/commands/remove-self.ts
@@ -6,7 +6,7 @@ export const data = new SlashCommandBuilder()
   .setName("removeself") // index import should be same as this
   .setDescription("remove yourself from the game before it starts");
 
-export async function execute(interaction: CommandInteraction, state: GameManagement) {
+export async function execute(interaction, state: GameManagement) {
   if (!state.activeGame) {
     return interaction.reply(constants.noActiveGame);
   }
