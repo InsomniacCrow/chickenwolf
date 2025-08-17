da527307712abe4b077cc7fb9871b0300a941a90
InsomniacCrow <143170718+InsomniacCrow@users.noreply.github.com>
2025-08-17 12:20:03 +1000

    That code can die so we no longer be messy
    (The mess is now elsewhere)

--- src/index.ts
+++ src/index.ts
@@ -35,43 +35,4 @@ client.on(Events.InteractionCreate, async (interaction: Interaction) => {
   }
 });
 
-client.on(Events.InteractionCreate, async (interaction: Interaction) => {
-  try {
-    if (!interaction.isUserSelectMenu() || !interaction.channel) {
-      return;
-    }
-    
-    if (interaction.customId === "test") {
-      
-      let message = await interaction.channel.messages.fetch(interaction.message.id);
-      let value = interaction.values;
-      //console.log(value);
-
-      //const selectedUsers = interaction.values; // Array of user IDs
-      console.log(value);
-      await interaction.deferUpdate()
-      await message.edit({
-        content: `✅ You selected: ${value}}`,
-      });
-      // if(value[0] === "charmander") {
-      //   await interaction.deferUpdate()
-        
-      //   await message.edit({content: "gasp, ch"})
-        
-      // } else if(value[0] === "squirtle") { 
-      //   await interaction.deferUpdate()
-        
-      //   await message.edit({content: "no way, sq", components: []})
-      // } 
-    
-  }
-  
-  // if error
-  } catch(e) {
-    console.error(e)
-    //interaction.followUp({content: e.message, ephemeral: true})
-  }
-
-})
-
 client.login(config.DISCORD_TOKEN);
